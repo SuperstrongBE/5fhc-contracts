67b4193579aff3538617f66b64d5ff8b56b56bc8
SuperstrongBE <remy@superstrong.be>
2022-04-13 23:49:29 +0200

    Dealing with hot fixes

--- fivefhc.contract.ts
+++ fivefhc.contract.ts
@@ -1,12 +1,10 @@
 
-import { Asset, Contract, Name, requireAuth, Symbol, TableStore } from 'proton-tsc';
-import { AllowContract } from 'proton-tsc/assembly/allow';
-import { sendMintAsset } from 'proton-tsc/assembly/atomicassets';
-import { AtomicAssets } from 'proton-tsc/assembly/contracts';
+import { Asset, Contract,Contracts, Name, requireAuth, Symbol, TableStore,ExtendedSymbol } from 'proton-tsc';
+
 import { Items } from './items.table'
 
 @contract
-export class fivefhc extends AllowContract {
+export class fivefhc extends Contracts.Allow.AllowContract {
 
   private itemTable:TableStore<Items> = Items.GetTable(this.receiver);
   private static FULL_PRICE:Asset = new Asset(10,new Symbol('foobar',6))
@@ -19,7 +17,7 @@ export class fivefhc extends AllowContract {
     requireAuth(this.receiver);
     const name:Name = new Name()
     const item:Items = new Items(name,978645123,owner,owner,rl_multiplier);
-    sendMintAsset(this.receiver,this.receiver,Name.fromString('6546546'),Name.fromString('6546546'),654987654,owner,[],[],[])
+    Contracts.AtomicAssets.sendMintAsset(this.receiver,this.receiver,Name.fromString('123azer'),Name.fromString('123azer'),1234,owner,[],[],[])
     this.itemTable.set(item,this.receiver);
     
 
