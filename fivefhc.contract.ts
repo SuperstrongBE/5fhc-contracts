e896e756d336d91db675b6233ea3f6b7b615f3b9
SuperstrongBE <remy@superstrong.be>
2022-06-08 18:50:43 +0200

    Add files via upload

--- fivefhc.contract.ts
+++ fivefhc.contract.ts
@@ -1,28 +1,112 @@
 
-import { Asset, Name, requireAuth, Symbol, TableStore, ExtendedAsset } from 'proton-tsc';
-import { sendMintAsset } from "./node_modules/proton-tsc/assembly/atomicassets"
-import { AllowContract } from "./node_modules/proton-tsc/assembly/allow"
-import { Items } from './items.table'
+import { Asset, Name, requireAuth, Symbol, TableStore, ExtendedAsset, print, printui, Contract } from 'proton-tsc';
+import { sendMintAsset, sendCreateTemplate, AtomicAttribute, Assets, sendCreateSchema, AtomicFormat, sendTransferNfts } from "proton-tsc/atomicassets"
+import { Items } from './items.table';
+
+import { sendTransferTokens } from 'proton-tsc/token';
 
 @contract
-export class fivefhc extends AllowContract {
+export class fivefhc extends Contract {
 
-  private itemTable:TableStore<Items> = Items.GetTable(this.receiver);
-  private static FULL_PRICE:Asset = new Asset(10000000,new Symbol('FOOBAR',6))
-  private static REDUCED_PRICE:Asset = new Asset(7000000,new Symbol('FOOBAR',6))
-    
+  private static FULL_PRICE: Asset = new Asset(10, new Symbol('XPR', 6));
+  private static REDUCED_PRICE: Asset = new Asset(7, new Symbol('XPR', 6));
+  private price: ExtendedAsset = new ExtendedAsset(fivefhc.FULL_PRICE, this.receiver);
+  private itemTable: TableStore<Items> = Items.GetTable(this.receiver);
+
 
   @action("mintitem")
-  mintitem(owner:Name, rl_multiplier:u32 ): void {
-    this.checkContractIsNotPaused();
-    requireAuth(this.receiver); 
-    const name:Name = new Name()
-    const authorizedMinter = Name.fromString('fivefhc')
-    const exAsset = new ExtendedAsset(fivefhc.FULL_PRICE,this.receiver);
-    const item:Items = new Items(name,978645123,owner,owner,rl_multiplier);
-    this.itemTable.set(item,this.receiver);
+  mintitem(owner: Name, collectionName: Name, schemaName: Name, immutableData: AtomicAttribute[], mutableData: AtomicAttribute[]): void {
+
+    requireAuth(owner);
+    print(">>>> Mint the assset");
     
-    //Contracts.Token.sendTransferTokens(owner,this.receiver,[exAsset],'');
-    sendMintAsset(this.contract,authorizedMinter,Name.fromString('524vigo'),Name.fromString('524vigo'),98766,owner,[],[],[]);
+    
+
+    sendTransferTokens(owner,Name.fromString('fivefhc'),[new ExtendedAsset(fivefhc.FULL_PRICE, Name.fromString("xtokens"))],"5FHC_MINT");
+    /*sendCreateSchema(this.receiver, this.receiver, collectionName, schemaName, [
+      new AtomicFormat("name","string" ),
+      new AtomicFormat("img","string" ),
+      new AtomicFormat("url","string" ),
+      new AtomicFormat("description","string" ),
+      new AtomicFormat("rl_multiplier","string" ),
+      new AtomicFormat("og_owner","string" )
+      
+    ]);*/
+    //sendCreateTemplate(this.receiver, this.receiver, collectionName, schemaName, false, true, 1, []);
+    //sendMintAsset(this.receiver, this.receiver, collectionName, schemaName, 1, owner, immutableData, mutableData, []);
   }
+
+  @action("logmint", notify)
+  logmint(
+    assetId: u64,
+    authorizedMinter: Name,
+    collectionName: Name,
+    schemaName: Name,
+    templateId: i32,
+    newAssetOwner: Name,
+    immutableData: AtomicAttribute[],
+    mutableData: AtomicAttribute[],
+    backedTokens: Asset[],
+    immutableTemplateData: AtomicAttribute[],
+  ): void {
+
+    print('reached the notify for fivefhc')
+    const assetTable: TableStore<Assets> = Assets.getTable(Name.fromString('atomicassets'), newAssetOwner);
+    assetTable.requireGet(assetId, 'No asset founds');
+    const existingItem = this.itemTable.lowerBound(assetId);
+    if (existingItem) {
+
+      print("Error the item could not exist")
+
+    } else {
+
+      print("Create new item")
+      if (!mutableData[0].value.get<u32>()) print("Missing required data in mutable")
+      const rlMultipler: u32 = mutableData[0].value.get<u32>();
+      printui(mutableData[0].value.get<u32>())
+      const newItem = new Items(
+        assetId,
+        newAssetOwner,
+        newAssetOwner,
+        rlMultipler
+      )
+      this.itemTable.set(newItem, this.receiver);
+
+
+    }
+
+  }
+
+  @action("transfer", notify)
+  onTransfer(from:Name,to:Name,amount:Asset,memo:string): void {
+
+    if(memo === "5FHC_MINT"){
+
+      print('>>>> Transfer nofitifcation');
+      print(memo);
+      print(">>>> Mint the assset");
+  
+
+    }
+    
+
+  }
+
+
+
+
+
+
+};
+
+@packer
+export class TokenTransfer extends ActionData {
+    constructor (
+        public from: Name = new Name(),
+        public to: Name = new Name(),
+        public quantity: Asset = new Asset(),
+        public memo: string = "",
+    ) {
+        super();
+    }
 }
--- playground.ts
+++ playground.ts
@@ -1,4 +1,6 @@
 import { Blockchain } from "@proton/vert";
+import { CollectionName } from "./fivefhc.constant";
+
 
 async function wait(ms: number) {
   return new Promise(resolve => {
@@ -8,14 +10,31 @@ async function wait(ms: number) {
 
 const main = async () => {
   const blockchain = new Blockchain();
+  
+  const xTokensContract = blockchain.createContract('xtokens', 'node_modules/proton-tsc/external/xtokens/xtokens', true);
+  //console.log (JSON.stringify(xTokensContract.actions));
+  const contract = blockchain.createContract('fivefhc', 'target/fivefhc.contract', true);
+  const atomicContract = blockchain.createContract('atomicassets', 'node_modules/proton-tsc/external/atomicassets/atomicassets', true);
+  const userAccount = blockchain.createAccount('johnson');
+  
+  //#################################################################################################################################
+  //Create and issue token 
+  await wait(0);
+  
+  await xTokensContract.actions.create([contract.name, '5000000000 XPR']).send(`${xTokensContract.name}@active`);
+  await xTokensContract.actions.issue([contract.name, '1000000000 XPR', '']).send(`${contract.name}@active`);
+  await xTokensContract.actions.transfer([contract.name,userAccount.name.toString(), '1000000000 XPR', 'funding']).send(`${contract.name}@active`);
+  await xTokensContract.actions.transfer([userAccount.name.toString(), contract.name.toString() ,'5 XPR', '5FHC_MINT']).send(`${userAccount.name}@active`);
 
-  const fivefhc = blockchain.createContract('fivefhc', 'target/fivefhc.contract',true);
-  const atomicAsset = blockchain.createContract('atomicassets', 'node_modules/proton-tsc/external/atomicassets/atomicassets',true);
+  //#################################################################################################################################
+  // Initialize atomicassets
+  // atomicassets Create collection
+  // atomicassets Create schema
   await wait(0);
 
-  // Initialize atomicassets
-  await atomicAsset.actions.init().send()
-  await atomicAsset.actions.admincoledit([
+  
+  await atomicContract.actions.init().send()
+  await atomicContract.actions.admincoledit([
     [
       { "name": "name", "type": "string" },
       { "name": "img", "type": "ipfs" },
@@ -24,9 +43,34 @@ const main = async () => {
     ]
   ]).send()
 
-  // Put you actions calls here
-  await atomicAsset.actions.createcol(['fivefhc','coolcool1234',true,['fivefhc'],['fivefhc'],0.15,[]]).send('fivefhc@active')
-  await fivefhc.actions.mintitem(['johnson',7]).send('fivefhc@active')
+  await atomicContract.actions.createcol(['fivefhc', CollectionName, true, ['fivefhc'], ['fivefhc'], 0.15, []]).send('fivefhc@active')
+  await atomicContract.actions.createschema(['fivefhc', CollectionName, CollectionName, [
+    { "name": "name", "type": "string" },
+    { "name": "img", "type": "ipfs" },
+    { "name": "description", "type": "string" },
+    { "name": "url", "type": "string" },
+    { "name": "rl_multiplyer", "type": "uint32" },
+    { "name": "og_owner", "type": "string" },
+
+  ]]).send('fivefhc@active');
+
+  //#################################################################################################################################
+  // Core contract calls
+  await wait(0);
+
+
+  await contract.actions.mintitem([userAccount.name, CollectionName, CollectionName,
+    [
+      { "key": "name", "value": ["string", "Cool NFT"] },
+      { "key": "description", "value": ["string", "a realy Cool NFT"] },
+      { "key": "img", "value": ["string", "QmP12BGM6pa9e427DVe8K77RjSUSLi6d1ZZsafJxWrvo7j"] },
+      { "key": "url", "value": ["string", "https://coolnft.io"] },
+
+    ],
+    [
+      { "key": "rl_multiplyer", "value": ["uint32", "7"] },
+    ]
+  ]).send('johnson@active')
 }
 
 main()
